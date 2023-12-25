0544546e31d08d0f6d9a18752d5cac60810d2e9a
Adriano Rios <adrianorios@hotmail.com.br>
2023-12-25 18:00:51 -0400

    adjusts main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsappclone/app.dart';
+import 'package:whatsappclone/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance
+      .collection("usuarios")
+      .doc('001')
+      .set({'nome': 'Adriano'});
   runApp(const App());
 }
